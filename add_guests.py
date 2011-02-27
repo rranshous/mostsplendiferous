1a06f910a8f53da4a214219c5b85b24afd08d9d7
Robby Ranshous <rranshous@gmail.com>
2011-02-27 05:14:43 +0000

    lil fixes, tool for adding guests from csv, starting to work on email

---
+++ add_guests.py
@@ -0,0 +1,25 @@
+
+COLUMNS = ('last_name','first_name','party_size',
+           'guests','notes','email_address')
+
+def get_data():
+    import csv
+    with file('./email_list.csv','rb') as fh:
+        reader = csv.reader(fh,delimiter=',',quotechar='"')
+        data = []
+        for row in reader:
+            data.append(dict(zip(COLUMNS,row)))
+    return data
+
+def add_guests():
+    data = get_data()
+    import models as m
+    guests = []
+    for d in get_data():
+        guests.append(m.Guest(
+            name = '%s %s' % (d.get('first_name',''),
+                              d.get('last_name','')),
+            guests_allowed = d.get('guests',0) or 0,
+            party_size = d.get('party_size',0) or 0
+        ))
+    return guests
--- email.py
+++
@@ -1,7 +0,0 @@
-
-
-def get_data():
-    # read in the data from our xls file
-
-    columns = ('last_name','first_names','party_size',
-               'side','email_address')
